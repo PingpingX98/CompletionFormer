9c1fd2a9fb2e0c90cfea529cca9a5efed9db0f0a
PingpingX98 <18786681463@163.com>
2025-07-28 08:09:37 +0800

    void_new

--- src/testing_scripts/test_void.sh
+++ src/testing_scripts/test_void.sh
@@ -3,17 +3,24 @@ test_augment=0
 optim_layer_input_clamp=1.0
 depth_activation_format='exp'
 
-ckpt=../checkpoints/NYU_generalization.pt
+ckpt=/home/descfly/Projects/CompletionFormer-main/NYUv2.pt
 
 for sample in 1500 500 150
+# do
+#   python main.py --dir_data /home/descfly/data/void_release/void_${sample} --data_name VOID \
+#     --gpus 0 --max_depth 5.0 \
+#     --GRU_iters $GRU_iters --optim_layer_input_clamp $optim_layer_input_clamp --depth_activation_format $depth_activation_format \
+#     --test_only --test_augment $test_augment --pretrain $ckpt \
+#     --log_dir /data/compare/metric/CF/experiments \
+#     --save "test_void${sample}" \
+#     --save_result_only
+# done
+
 do
-  python main.py --dir_data ../datasets/void_release/void_${sample} --data_name VOID \
-    --gpus 0 --max_depth 5.0 \
-    --GRU_iters $GRU_iters --optim_layer_input_clamp $optim_layer_input_clamp --depth_activation_format $depth_activation_format \
-    --test_only --test_augment $test_augment --pretrain $ckpt \
-    --log_dir ../experiments/ \
-    --save "test_void${sample}" \
-    --save_result_only
+    python main.py --dir_data /home/descfly/data/void_release/void_${sample} \
+        --data_name VOID  \
+        --gpus 0 --max_depth 5.0 --num_sample $sample \
+        --test_only --pretrain $ckpt --batch_size 1 \
+        --log_dir /data/compare/metric/CFormer/experiments/ \
+        --save "test_void${sample}" 
 done
-
-
