e4eae67135757013da08240e498e13dc3dc2ee15
PingpingX98 <18786681463@163.com>
2025-07-28 18:03:50 +0800

    test_kitti

--- src/testing_scripts/test_kitti.sh
+++ src/testing_scripts/test_kitti.sh
@@ -7,11 +7,12 @@ pred_confidence_input=1
 
 
 ckpt=/home/descfly/Projects/CompletionFormer-main/KITTIDC_L1L2.pt
-for lidar_lines in 4 8 16 32 64 
+# for lidar_lines in 4 8 16 32 64 
+for lidar_lines in 64 
 do
-  python main.py --dir_data /data/kitti_depth \
+  python main_time.py --dir_data /home/descfly/data/kitti_depthh \
       --data_name KITTIDC --split_json ../data_json/kitti_dc.json \
-      --patch_height 240 --patch_width 1216 --gpus 0 --max_depth 90.0 \
+      --patch_height 352 --patch_width 1216 --gpus 0 --max_depth 90.0 \
       --top_crop 0 --test_crop  --test_only \
       --pretrain $ckpt  --lidar_lines $lidar_lines \
       --log_dir /data/compare/metric/CFormer/experiments/ \
